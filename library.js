ad8a65dcdb1b6ed70c2f593941fec43bc29e489d
William Karsten <yme_ynot45@hotmail.com>
2022-01-13 14:46:51 -0600

    switched to the constructor instead of seeding the array.

--- library.js
+++ library.js
@@ -1,14 +1,11 @@
-let myLibrary = [
-    {"title": "The Hobbit", "author": "J.R.R. Tolkien", "pageNumber": 295, "haveRead": false},
-    {"title": "Flight from the Dark", "author": "Joe Denver", "pageNumber": 300, "haveRead": true},
-    {"title": "Fire on the Water", "author": "Gary Chalk", "pageNumber": 350, "haveRead": false},
-];
+let myLibrary = [];
 
 const Book = function (title, author, pageNumber, haveRead) {
     this.title = title;
     this.author = author;
     this.pageNumber = pageNumber;
     this.haveRead = haveRead;
+    myLibrary.push(this);
 }
 
 // const addBookToLibrary = function (myLibrary, newBook) {
@@ -16,24 +13,27 @@ const Book = function (title, author, pageNumber, haveRead) {
 //     return myLibrary
 // }
 
-const loadLibrary = function(library) {
+const loadLibrary = function(library) {  //puting this aside for the moment to actually use the constructor
     frame = document.getElementById("shelf");
-    // myLibrary.forEach(book => {
-        let book = document.createElement("li");
-        book.setAttribute("class", "book");
-        book.textContent = "Happy";
-        frame.appendChild(book);
-        console.log(library);
-    // });
+    library.forEach(book => {
+        let bookSlot = document.createElement("li");
+        bookSlot.setAttribute("class", "book");
+        bookSlot.textContent = Object.getOwnPropertyNames(book);
+        frame.appendChild(bookSlot);
+    });
 
-    // for (let i = 0; i < myLibrary.lenght; i++) {
-    //     let bookSlot = document.createElement("li");
-    //     bookSlot.className = "bookSlot";
-    //     frame.appendChild(bookSlot);
-    //     alert()
-    // }
     // Not done either need to then post the data to each of the slots or fill the slots as I go.
     // most likely a myLibrary.forEach
 }
+
+// {"title": "The Hobbit" , "author": "J.R.R. Tolkien", "pageNumber": 295, "haveRead": false},
+// {"title": "Flight from the Dark", "author": "Joe Denver", "pageNumber": 300, "haveRead": true},
+// {"title": "Fire on the Water", "author": "Gary Chalk", "pageNumber": 350, "haveRead": false},
+
+Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+Book("Flight from the Dark", "Joe Denver", 300, true);
+Book("Fire on the Water", "Gary Chalk", 350, false);
+
+
 
 document.body.onload = () => loadLibrary(myLibrary);
